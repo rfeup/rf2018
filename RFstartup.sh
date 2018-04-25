577961e41f066bf8b630f168010c0922c8246eae
rfeup <rfeup2017@gmail.com>
2018-04-25 21:20:54 +0000

    Added script to launch all the executables

---
+++ RFstartup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# My first script
+
+echo "Hello World!"
+
+cd /home/pi/Desktop/R@F_2018
+
+BatRoverPC2018/batroverpc &
+ConnectLaser2018/laser &
+HAL2018/NetLaserLoc &
+ps3cam.2017/ps3cam
+
+
